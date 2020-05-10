52fed83afd25c5bfc0e0130308ed433c2f618e5c
Gabe Wyatt <gabe@solarixdigital.com>
2020-05-10 12:57:03 -0700

    Removed widget deploy permission changes (Laravel can access just fine without 'www-data' group).

--- .deploy/all/deploy.sh
+++ .deploy/all/deploy.sh
@@ -3,12 +3,3 @@
 # Move widget build to proper remote directory
 echo "Deploying ${WIDGET_PATH} to remote Dashboard."
 scp -o StrictHostKeyChecking=no "${WIDGET_PATH}" "ubuntu@${DEPLOY_ENDPOINT}:${TARGET_DIRECTORY}"
-
-# Update permissions
-ssh -o StrictHostKeyChecking=no ubuntu@"${DEPLOY_ENDPOINT}" << EOF
-  LARAVEL_OWNER=ubuntu
-  LARAVEL_WS_GROUP=www-data
-
-  sudo usermod -a -G "\${LARAVEL_WS_GROUP}" "\${LARAVEL_OWNER}"
-  sudo chown -R "\${LARAVEL_OWNER}:\${LARAVEL_WS_GROUP}" "\${TARGET_DIRECTORY}"
-EOF
